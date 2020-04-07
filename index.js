b5a7b8c06b4946e75435b0e958daae752972ceeb
Sushil Shinde <shinde.sushil@gmail.com>
2020-04-08 01:19:10 +0530

    added api endpoint

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ var serveStatic = require('serve-static')
 app.use(bp.urlencoded({ extended: false }));
 app.use(serveStatic(path.join(__dirname)));
 
+app.use('/api/covid-data',(req,res) => {
+    res.send({message:"hello"});
+})
+
 app.use('/',(req,res) => {
     res.sendFile('index.html', { root: __dirname });
 })
+
+
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
